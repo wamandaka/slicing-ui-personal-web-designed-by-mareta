d85d0fe21af7e178ab482bbbbbb09a37b331bbc3
wamandaka <108261088+wamandaka@users.noreply.github.com>
2023-12-28 21:37:12 +0700

    update hero img size

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,23 +8,23 @@ const Hero = () => {
         id="about"
         className="flex flex-col-reverse sm:flex-row pt-24 px-4 mx-auto sm:px-16 sm:pt-36 md:px-20"
       >
-        <div className="py-5">
+        <div className="py-5 lg:w-[50%] lg:pt-24">
           <p className="font-bold mt-3">Mareta Ayu Handanari</p>
           <h1 className="text-4xl py-4 bg-gradient-to-b from-[#FFBDA0] to-[#FF6CD6] bg-clip-text text-transparent font-extrabold">
             Front-end Developer
           </h1>
-          <p className="mb-10">
+          <p className="mb-10 lg:text-xl">
             I design delightful product & code web. I can help everyone build
             their business. it such a happiness for me. so, let me to help you!
           </p>
           <a
             href="#"
-            className="py-2 px-7 bg-gradient-to-r from-[#256BF4] to-[#3AC1DE] text-white rounded-full hover:shadow-md hover:shadow-[#3AC1DE] duration-300"
+            className="py-3 px-10 bg-gradient-to-r from-[#256BF4] to-[#3AC1DE] text-white rounded-full hover:shadow-md hover:shadow-[#3AC1DE] duration-300"
           >
             Contact Me
           </a>
         </div>
-        <img src={imgHero} alt="" className="w-[50%] mx-auto drop-shadow-2xl" />
+        <img src={imgHero} alt="" className="w-[50%] mx-auto drop-shadow-2xl md:w-[50%] md:h-[50%]" />
       </section>
     </>
   );
